1a9375da54a6576562b60cef07ffdb19884acc14
Jack Danger Canty <git@6brand.com>
2008-04-21 22:47:35 +0000

    better looking sidebar

--- views/javascript.js
+++ views/javascript.js
@@ -8,11 +8,13 @@ $(function(){
       $('status').text = 'currently: developing software'
     },
     success: function(xml){
-      twitters = $(xml).find('entry');
+      twitters = $(xml).find('title');
       var i = 0;
       twitters.each(function(){
-        text = $(this).find('title').text().replace(/^jackdanger: /, '')
+        text = $(this).text().replace(/^jackdanger: /, '')
         if(0 == i)
+          $('#twitters').append($('<h3></h3>').append('<a href="http://twitter.com/jackdanger/">'+text+'</a>'))
+        else if(1 == i)
           $('#status').text(text);
 	else if(19 == i)
 	  $('#twitters').append('<div class="alt last">'+text+'</div>');
