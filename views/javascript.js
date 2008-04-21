9983c9ca0d473c0295047e3c39499e507905357a
Jack Danger Canty <git@6brand.com>
2008-04-21 06:33:14 +0000

    initial attempt at importing twitter status

--- views/javascript.js
+++ views/javascript.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-
+$(function(){
+  $.ajax({
+    url: 'http:///twitter.com/statuses/user_timeline/3496901.rss',
+    type: 'GET',
+    dataType: 'xml',
+    timeout: 1000,
+    error: function(){
+      alert('couldn\'t retrieve status')
+    },
+    success: function(xml){
+      $(xml).find('entry').each(function(){
+        var status_text = $(this).title().gsub(/$jackdanger: /, '')
+	$('#status').text = status_text;
+      });
+    }
+  })
 })
