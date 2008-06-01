85de24a9ae2c8974578c0891b5c52bbfe2993ac9
Jack Danger Canty <git@6brand.com>
2008-06-01 19:14:31 +0000

    Attempting to use views in .scm

--- app.scm
+++ app.scm
@@ -1,2 +1,4 @@
-(defresource "/" (quote (html (body (h1 "Bus Scheme") ) )))
+(define index (load "views/index.xml.scm"))
+;; (defresource "/" (quote (html (body (h1 "Bus Scheme") ) )))
+(defresource "/" 'index) 
 (webwait)
