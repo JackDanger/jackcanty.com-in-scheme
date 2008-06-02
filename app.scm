ef8fbb4073fbe96d29a13b0f8fc1d8c64d204595
Jack Danger Canty <git@6brand.com>
2008-06-02 16:49:38 -0700

    Adding comment for what url needs to be retrieved

--- app.scm
+++ app.scm
@@ -2,6 +2,8 @@
 (defwebapp "/jquery.js"       (lambda (env) (list 200 (quote ("Content-Type" "text/javascript")) (read "views/jquery.js"))))
 (defwebapp "/blueprint.css"   (lambda (env) (list 200 (quote ("Content-Type" "text/css"))        (read "views/blueprint.css"))))
 (defwebapp "/stylesheet.css"  (lambda (env) (list 200 (quote ("Content-Type" "text/css"))        (read "views/stylesheet.css"))))
+
+; http://api.flickr.com/services/feeds/photoset.gne?set=72157605182324135&nsid=30691816@N00&lang=en-us&format=rss_200
 (defwebapp "/dailyoffice.xml" (lambda (env) (list 200 (quote ("Content-Type" "application/xml")) (read "views/dailyoffice.xml"))))
 (defwebapp "/twitter.xml"     (lambda (env) (list 200 (quote ("Content-Type" "application/xml")) (read "views/twitter.xml"))))
 
